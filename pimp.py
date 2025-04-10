e9372de56cf5467839fd227ea6a80ad8c8664399
sap-ai <adriangerleit@gmail.com>
2025-04-10 16:36:14 -0400

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -1,7 +1,15 @@
 import sys
-import urllib.request
-download = ''
-package = sys.argv[1]
-dest = sys.argv[2]
-download = 'https://github.com/sap-ai/pimpstore/' + package
-urllib.request.urlretrieve(download, dest)
+import requests
+def download_large_file(url, destination):
+    try:
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            with open(destination, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+        print("Package downloaded successfully!")
+    except requests.exceptions.RequestException as e:
+        print("Error downloading the Package:", e)
+dest = sys.argv[1]
+package = 'https://github.com/sap-ai/pimpstore/' + dest
+download_large_file(package, dest)
