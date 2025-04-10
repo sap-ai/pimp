0a9cf2554d5c8cfa62ff03f3245a3ec269123792
sap-ai <adriangerleit@gmail.com>
2025-04-10 17:35:38 -0400

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -6,5 +6,19 @@ package = 'https://github.com/sap-ai/pimpstore/blob/main/' + dest
 fl = requests.get(package)
 soup = BeautifulSoup(fl.content, 'html.parser')
 d = soup.find('textarea')
+li = []
+s = ""
+fnl = ""
+for link in soup.find_all('textarea'):
+   try:
+      li = link.split(">")
+      s = ''.join(li)
+      li = s.split("<")
+      s = ''.j(li)
+       try:
+         exec(s)
+         fnl = li[0]
+      except:
+         pass
 with open(dest, 'w') as file:
-    file.write(str(d))
+   file.write(str(fnl))
