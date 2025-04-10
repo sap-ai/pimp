3e484b5421f42517e2d48d47ae2eb52b5c52c906
sap-ai <adriangerleit@gmail.com>
2025-04-10 17:06:05 -0400

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 dest = sys.argv[1]
 package = 'https://github.com/sap-ai/pimpstore/blob/main/' + dest
 fl = requests.get(package)
-soup = BeautifulSoup(fl, 'html.parser')
+soup = BeautifulSoup(fl.content, 'html.parser')
 d = soup.find('textarea')
 with open(dest, 'w') as file:
     file.write(d)
